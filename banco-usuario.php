ad5f0a6330965a11f6d1276028dbb4655280ccc6
Rodrigo Rosa <rodrigo@wonder.com.br>
2017-01-02 14:29:18 -0200

    Adicionado tela de login - inicio da segunda parte

---
+++ banco-usuario.php
@@ -0,0 +1,8 @@
+<?php
+
+function buscaUsuario($conexao, $email, $senha) {
+  $senhaMD5 = md5($senha);
+  $query = "select * from usuarios where email = '{$email}' and senha = '{$senhaMD5}'";
+  $resultado = mysqli_query($conexao, $query);
+  return mysqli_fetch_assoc($resultado);
+}
--- index.php
+++ index.php
@@ -1,5 +1,35 @@
 <?php include 'cabecalho.php'; ?>
 
-  <h1>Bem vindo</h1>
+<?php
+if(isset($_GET["login"]) && $_GET["login"]==true) {
+?>
+<p class="alert-success">Logado com sucesso!</p>
+<?php
+}
+?>
+<?php
+if(isset($_GET["login"]) && $_GET["login"]==false) {
+?>
+<p class="alert-danger">Usuário ou senha inválida!</p>
+<?php
+}
+?>
+
+<h1>Bem vindo</h1>
+
+<h2>Login</h2>
+<form action="login.php" method="post">
+  <div class="form-group">
+    <label for="email">Email</label>
+    <input type="email" name="email" class="form-control" id="email">
+  </div>
+
+  <div class="form-group">
+    <label for="senha">Senha</label>
+    <input type="password" name="senha" class="form-control" id="senha">
+  </div>
+
+  <button class="btn btn-primary">Login</button>
+</form>
 
 <?php include 'rodape.php'; ?>
